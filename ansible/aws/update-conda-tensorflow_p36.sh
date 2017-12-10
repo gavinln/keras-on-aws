3079b8386330bc34adbba50bd2042cd81e2d4609
Gavin Noronha <gavinln@hotmail.com>
2017-12-10 11:39:56 -0800

    update setup instructions

--- ansible/aws/update-conda-tensorflow_p36.sh
+++ ansible/aws/update-conda-tensorflow_p36.sh
@@ -3,6 +3,7 @@
 CONDA_ROOT=/home/ubuntu/anaconda3
 
 source $CONDA_ROOT/bin/activate tensorflow_p36
+python -c "import matplotlib; matplotlib.get_cachedir()"
 pip install sklearn
 pip install seaborn
 
