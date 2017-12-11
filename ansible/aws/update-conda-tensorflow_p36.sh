522756171f1103e2608a47abb1f121656e5216ac
Gavin Noronha <gavinln@hotmail.com>
2017-12-10 22:33:47 -0800

    updated notebooks and improved doc

--- ansible/aws/update-conda-tensorflow_p36.sh
+++ ansible/aws/update-conda-tensorflow_p36.sh
@@ -3,8 +3,11 @@
 CONDA_ROOT=/home/ubuntu/anaconda3
 
 source $CONDA_ROOT/bin/activate tensorflow_p36
-python -c "import matplotlib; matplotlib.get_cachedir()"
+python -c "import matplotlib; matplotlib.get_cachedir()"  # does not create cache
 pip install sklearn
 pip install seaborn
 
-# pip install jupyter_contrib_nbextensions
+pip install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
+cd $(jupyter --data-dir)/nbextensions
+git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
