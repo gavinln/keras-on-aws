f0026472f1d459f71f493e410c1a75b7fc66558d
Gavin Noronha <gavinln@hotmail.com>
2017-12-14 22:36:57 -0800

    updated to display progress and the confusion matrix

--- ansible/aws/update-conda-tensorflow_p36.sh
+++ ansible/aws/update-conda-tensorflow_p36.sh
@@ -6,8 +6,11 @@ source $CONDA_ROOT/bin/activate tensorflow_p36
 python -c "import matplotlib; matplotlib.get_cachedir()"  # does not create cache
 pip install sklearn
 pip install seaborn
+pip install keras-tqdm
 
 pip install jupyter_contrib_nbextensions
 jupyter contrib nbextension install --user
 cd $(jupyter --data-dir)/nbextensions
-git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+if [[! -d vim_binding ]]; then
+    git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+fi
